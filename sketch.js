12f896db7f2fd54be6dc002c4b69a93e62f5a9f4
Tyrel Rucker <trucker@wlapcs.org>
2018-01-22 18:56:36 +0000

    added sliders anad background changing

--- sketch.js
+++ sketch.js
@@ -1,8 +1,23 @@
+var ruckerSlider, genaeSlider, bakerSlider;
+
 function setup(){
     createCanvas(600,400);
     background(0);
+    
+    ruckerSlider = createSlider(0.255,120);
+    ruckerSlider.position(20,20)
+     
+     ganaeSlider = createSlider(0.255,120);
+    genaeSlider.position(20,50)
+    
+     bakerSlider = createSlider(0.255,120);
+    bakerSlider.position(20,80)
 }
 
 function draw(){
+    redCreep = ruckerSlider.value();
+    greenSavage = genaeSlider.value();
+    bluekiller = bakerSlider.value();
     
+    background(redCreep,greenSavage,bluekiller)
 }
